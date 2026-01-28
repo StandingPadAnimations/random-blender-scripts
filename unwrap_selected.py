d6d3af780e8c74ead444e857cb30def20574b746
Maryam Sheikh <mahid@standingpad.org>
2026-01-27 19:34:41 -0600

    Rework Python implementation of UV unwrapping

--- unwrap_selected.py
+++ unwrap_selected.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025 Maryam Sheikh (Mahid Sheikh) <mahid@standingpad.org>
+# Copyright (C) 2025-2026 Maryam Sheikh (Mahid Sheikh) <mahid@standingpad.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import enum
+
 import bpy
 import bmesh
 import mathutils
@@ -21,10 +23,9 @@ SELECTED = (obj for obj in bpy.context.selected_objects)
 
 
 # The cube projection code is a translation of
-# Blender's C++ code to Python, done by Gemini
+# Blender's C++ code to Python.
 #
-# Reference
-# https://projects.blender.org/blender/blender/src/commit/e1e3e7e50694de20a3bac6889867ecc770305e2c/source/blender/editors/uvedit/uvedit_unwrap_ops.cc#L4181-L4227
+# Reference Commit: e1e3e7e50694de20a3bac6889867ecc770305e2c
 #
 # We have our own implementation of the operator to
 # performance issues associated with using operators
@@ -32,88 +33,378 @@ SELECTED = (obj for obj in bpy.context.selected_objects)
 # door for parallel processing of meshes, though that
 # may not be necessary.
 #
-# In testing, this is virtually identical results wise
-# to the built-in cube projection
-def get_dominant_axis_indices(normal: mathutils.Vector) -> tuple[int, int]:
+# While it's as close to 1-to-1 as possible, some liberties
+# have been taken to keep things minimal. Function names
+# are the same as they would be in the Blender source code.
+# Comments will contain the source file and lines of the original
+# C++ functions.
+
+# ./source/blender/blenlib/BLI_utildefines.h
+# Lines: 40-51
+INIT_MIN = 1.0e30
+INIT_MAX = -1.0e30
+
+INIT_MINMAX = ((INIT_MIN, INIT_MIN, INIT_MIN), (INIT_MAX, INIT_MAX, INIT_MAX))
+INIT_MINMAX2 = ((INIT_MIN, INIT_MIN), (INIT_MAX, INIT_MAX))
+
+FloatBounds3 = tuple[float, float, float]
+FloatBounds2x3 = tuple[FloatBounds3, FloatBounds3]
+
+
+class V3DAround(enum.Enum):
+    CENTER_BOUNDS = 0
+    CENTER_MEDIAN = 1
+    CURSOR = 2
+    ACTIVE = 3
+    LOCAL_ORIGINS = 4
+
+
+def get_v3d_pivot() -> V3DAround:
+    """Return the current pivot in View3D."""
+    match bpy.context.scene.tool_settings.transform_pivot_point:
+        case "MEDIAN_POINT":
+            return V3DAround.CENTER_MEDIAN
+        case "INDIVIDUAL_ORIGINS":
+            return V3DAround.LOCAL_ORIGINS
+        case "ACTIVE_ELEMENT":
+            return V3DAround.ACTIVE
+        case "CURSOR":
+            return V3DAround.CURSOR
+        case "BOUNDING_BOX_CENTER":
+            return V3DAround.CENTER_BOUNDS
+    return V3DAround.CENTER_BOUNDS
+
+
+def minmax_v3v3_v3(
+    f_min: FloatBounds3, f_max: FloatBounds3, vec: FloatBounds3
+) -> FloatBounds2x3:
+    """Translation of Blender's minmax_v3v3_v3 function.
+
+    ./source/blender/blenlib/intern/math_vector.cc
+    Lines: 716-725
     """
-    Determines which axes (x, y) to use for projection based on the
-    largest component of the face normal.
+    new_f_min = (min(f_min[0], vec[0]), min(f_min[1], vec[1]), min(f_min[2], vec[2]))
+    new_f_max = (max(f_max[0], vec[0]), max(f_max[1], vec[1]), max(f_max[2], vec[2]))
+
+    return new_f_min, new_f_max
+
+
+def add_v3_v3(r: FloatBounds3, a: FloatBounds3) -> FloatBounds3:
+    """Translation of Blender's add_v3_v3 function.
+
+    ./source/blender/blenlib/intern/math_vector_inline.cc
+    Lines: 280-285
     """
-    abs_x = abs(normal.x)
-    abs_y = abs(normal.y)
-    abs_z = abs(normal.z)
-
-    # Replicating BLI_math_geom.c logic
-    if abs_x > abs_y:
-        if abs_x > abs_z:
-            return 1, 2  # X is dominant, project on Y, Z
-        else:
-            return 0, 1  # Z is dominant, project on X, Y
-    else:
-        if abs_y > abs_z:
-            return 0, 2  # Y is dominant, project on X, Z
-        else:
-            return 0, 1  # Z is dominant, project on X, Y
+    return (
+        r[0] + a[0],
+        r[1] + a[1],
+        r[2] + a[2],
+    )
 
 
-def calculate_selection_center(
-    bm: bmesh.types.BMesh, use_select: bool
-) -> mathutils.Vector:
-    """Calculates the average center of selected faces to use as the projection pivot."""
-    total_co = mathutils.Vector((0.0, 0.0, 0.0))
-    count = 0
+def sub_v3_v3v3(a: FloatBounds3, b: FloatBounds3) -> FloatBounds3:
+    """Translation of Blender's sub_v3_v3v3 function.
 
-    for face in bm.faces:
-        if use_select and not face.select:
-            continue
-        # Use face center (centroid) for average
-        total_co += face.calc_center_median()
-        count += 1
-
-    if count == 0:
-        return mathutils.Vector((0.0, 0.0, 0.0))
-
-    return total_co / count
+    ./source/blender/blenlib/intern/math_vector_inline.cc
+    Lines: 354-359
+    """
+    return (a[0] - b[0], a[1] - b[1], a[2] - b[2])
 
 
-def cube_project(
-    bm: bmesh.types.BMesh,
-    cube_size: float = 1.0,
-    use_select: bool = True,
-    center: mathutils.Vector | None = None,
+def mul_v3_fl(r: FloatBounds3, f: float) -> FloatBounds3:
+    """Translation of Blender's mul_v3_fl function.
+
+    ./source/blender/blenlib/intern/math_vector_inline.cc
+    Lines: 408-413
+    """
+    return (r[0] * f, r[1] * f, r[2] * f)
+
+
+def BM_face_calc_bounds_expand(
+    bm_face: bmesh.types.BMFace, f_min: FloatBounds3, f_max: FloatBounds3
+) -> FloatBounds2x3:
+    """Translation of Blender's BM_face_calc_bounds_expand function.
+
+    ./source/blender/bmesh/intern/bmesh_polygon.cc
+    Lines: 607-614
+    """
+    cur_f_min, cur_f_max = f_min, f_max
+    for loop in bm_face.loops:
+        cur_f_min, cur_f_max = minmax_v3v3_v3(cur_f_min, cur_f_max, loop.vert.co)
+    return cur_f_min, cur_f_max
+
+
+def ED_uvedit_get_aspect_from_material(
+    obj: bpy.types.Object, material_index: int
+) -> tuple[float, float]:
+    """Translation of Blender's ED_uvedit_get_aspect_from_material function, with other functions inlined.
+
+    ./source/blender/editors/uvedit/uvedit_unwrap_ops.cc
+    Lines: 494-507
+
+    ./source/blender/editors/space_image/image_edit.cc
+    Lines: 311-326
+    """
+    r_aspx, r_aspy = 1.0, 1.0
+    if material_index < 0 or material_index >= len(obj.material_slots):
+        return r_aspx, r_aspy
+
+    mat = obj.material_slots[material_index].material
+    if mat:
+        active_node = mat.node_tree.nodes.active
+        if not active_node or active_node.bl_idname != "TEX_IMAGE":
+            return r_aspx, r_aspy
+
+        image = active_node.image
+
+        if image:
+            w, h = image.size[0], image.size[1]
+            r_aspx, r_aspy = image.display_aspect[0], image.display_aspect[1]
+
+            r_aspx *= float(w)
+            r_aspy *= float(h)
+
+    return r_aspx, r_aspy
+
+
+def shrink_loop_uv_by_aspect_ratio(
+    face: bmesh.types.BMFace, uv_layer: bmesh.types.BMLayerItem, aspect_y: float
 ) -> None:
-    """
-    Python equivalent of Blender's Cube Projection operator, so
-    we don't have to use operators (and their performance impact)
-    """
-    loc = center
-    if center is None:
-        loc = calculate_selection_center(bm, use_select)
+    """Translation of Blender's shrink_loop_uv_by_aspect_ratio function.
 
-    # Prevent division by zero
+    ./source/blender/editors/uvedit/uvedit_unwrap_ops.cc
+    Lines: 2504-2524
+    """
+    assert aspect_y != 1.0  # Nothing to do, should be handled by caller.
+    assert aspect_y > 0.0  # Negative aspect ratios are not supported
+
+    for loop in face.loops:
+        loop_uv = loop[uv_layer]
+
+        if aspect_y > 1.0:
+            loop_uv.uv[0] = loop_uv.uv[0] / aspect_y + (0.5 - 0.5 / aspect_y)
+        else:
+            loop_uv.uv[1] = loop_uv.uv[1] * aspect_y + (0.5 - 0.5 * aspect_y)
+
+
+def correct_uv_aspect_per_face(obj: bpy.types.Object, bm: bmesh.types.BMesh) -> None:
+    """Translation of Blender's correct_uv_aspect_per_face function.
+
+    ./source/blender/editors/uvedit/uvedit_unwrap_ops.cc
+    Lines: 2543-2584
+    """
+    materials_num = len(obj.material_slots)
+    if materials_num == 0:
+        return
+
+    material_aspect_y: list[float] = [-1.0] * materials_num
+
+    for face in bm.faces:
+        if not face.select:
+            continue
+
+        material_index = face.material_index
+        if materials_num < 0 or material_index >= materials_num:
+            continue
+
+        aspect_y: float = material_aspect_y[material_index]
+
+        if aspect_y == -1.0:
+            aspx, aspy = ED_uvedit_get_aspect_from_material(obj, material_index)
+            aspect_y = aspx / aspy
+            material_aspect_y[material_index] = aspect_y
+
+        # Not an elif since we need to
+        # check this after calculating
+        # aspect_y
+        if aspect_y == 1.0:
+            continue  # Scaling by 1.0 has no effect
+
+        uv_layer = bm.loops.layers.uv.verify()
+        shrink_loop_uv_by_aspect_ratio(face, uv_layer, aspect_y)
+
+
+def uv_map_clip_correct(
+    obj: bpy.types.Object, bm: bmesh.types.BMesh, per_face_aspect: bool
+) -> None:
+    """Heavially simplified translation of Blender's uv_map_clip_correct function."""
+
+    dx, dy = 0.0, 0.0
+    min, max = INIT_MINMAX2
+    correct_aspect: bool = True
+
+    if correct_aspect and per_face_aspect:
+        correct_uv_aspect_per_face(obj, bm)
+
+
+def uv_map_transform_calc_bounds(
+    bm: bmesh.types.BMesh, r_min: FloatBounds3, r_max: FloatBounds3
+) -> FloatBounds2x3:
+    """Translation of Blender's uv_map_transform_calc_bounds function.
+
+    ./source/blender/editors/uvedit/uvedit_unwrap_ops.cc
+    Lines: 2275-2285
+    """
+    cur_r_min, cur_r_max = r_min, r_max
+    for face in bm.faces:
+        if not face.select:
+            continue
+        cur_r_min, cur_r_max = BM_face_calc_bounds_expand(face, cur_r_min, cur_r_max)
+    return cur_r_min, cur_r_max
+
+
+def uv_map_transform_calc_center_median(
+    bm: bmesh.types.BMesh, r_center: FloatBounds3
+) -> FloatBounds3:
+    """Translation of Blender's uv_map_transform_calc_center_median function.
+
+    ./source/blender/editors/uvedit/uvedit_unwrap_ops.cc
+    Lines: 2287-2302
+    """
+    cur_r_center: FloatBounds3 = (0, 0, 0)
+    center_accum_num = 0
+    for face in bm.faces:
+        if not face.select:
+            continue
+        center = face.calc_center_median()
+        cur_r_center = add_v3_v3(cur_r_center, center)
+        center_accum_num += 1
+    return mul_v3_fl(cur_r_center, 1.0 / center_accum_num)
+
+
+def uv_map_transform_center(
+    object: bpy.types.Object, bm: bmesh.types.BMesh, r_center: FloatBounds3
+) -> tuple[FloatBounds3, FloatBounds2x3]:
+    """Translationof Blender's uv_map_transform_center function.
+
+    ./source/blender/editors/uvedit/uvedit_unwrap_ops.cc
+    Lines: 2304-2359
+    """
+    bounds: FloatBounds2x3 = INIT_MINMAX
+    cur_r_center = r_center
+    around: V3DAround = get_v3d_pivot()
+    is_minmax_set: bool = False
+
+    # TODO: Implement support for CURSOR and ACTIVE
+    match around:
+        case V3DAround.CENTER_BOUNDS:
+            bounds = uv_map_transform_calc_bounds(bm, bounds[0], bounds[1])
+            is_minmax_set = True
+            bounds = minmax_v3v3_v3(r_center, bounds[0], bounds[1])
+        case V3DAround.CENTER_MEDIAN:
+            cur_r_center = uv_map_transform_calc_center_median(bm, r_center)
+        case V3DAround.CURSOR:
+            pass
+        case V3DAround.ACTIVE:
+            pass
+        case V3DAround.LOCAL_ORIGINS:
+            cur_r_center = (0, 0, 0)
+        case _:
+            cur_r_center = (0, 0, 0)
+
+    if not is_minmax_set:
+        bounds = uv_map_transform_calc_bounds(bm, bounds[0], bounds[1])
+
+    return cur_r_center, bounds
+
+
+def axis_dominant_v3(
+    r_axis_a: int, r_axis_b: int, axis: FloatBounds3
+) -> tuple[int, int]:
+    """Translation of Blender's axis_dominant_v3 function.
+
+    ./source/blender/blenlib/intern/math_geom_inline.cc
+    Lines: 43-61
+    """
+    xn, yn, zn = abs(axis[0]), abs(axis[1]), abs(axis[2])
+
+    if zn >= xn and zn >= yn:
+        return 0, 1
+    elif yn >= xn and yn >= zn:
+        return 0, 2
+    return 1, 2
+
+
+def uvedit_unwrap_cube_project(
+    bm: bmesh.types.BMesh,
+    cube_size: float,
+    use_select: bool,
+    only_selected_uvs: bool,
+    center: FloatBounds3 | None = None,
+) -> None:
+    """Translation of uvedit_unwrap_cube_project.
+
+    ./source/blender/editors/uvedit/uvedit_unwrap_ops.cc
+    Lines: 4181-4227
+    """
+    cur_cube_size = cube_size
+    loc: FloatBounds3 = (0, 0, 0)
+    cox, coy = 0, 0
+
+    if center is not None:
+        loc = center
+
     if cube_size == 0.0:
-        cube_size = 1.0
-
-    scale_inv = 1.0 / cube_size
-    uv_layer = bm.loops.layers.uv.verify()
+        cur_cube_size = 1.0
 
     for face in bm.faces:
         if use_select and not face.select:
             continue
+        elif only_selected_uvs and not face.uv_select:
+            continue
 
-        # Determine which plane to map this face onto
-        cox, coy = get_dominant_axis_indices(face.normal)
+        cox, coy = axis_dominant_v3(cox, coy, face.normal)
+        uv_layer = bm.loops.layers.uv.verify()
 
         for loop in face.loops:
             co = loop.vert.co
             loop_uv = loop[uv_layer]
 
-            # Project coordinate to 0-1 UV space
-            # 0.5 centers the projection on the UV tile
-            u = 0.5 + ((co[cox] - loc[cox]) * scale_inv)
-            v = 0.5 + ((co[coy] - loc[coy]) * scale_inv)
+            u = 0.5 + ((co[cox] - loc[cox]) / cur_cube_size)
+            v = 0.5 + ((co[coy] - loc[coy]) / cur_cube_size)
 
             loop_uv.uv = (u, v)
+
+
+def cube_project_exec(cube_size: float, objects: tuple[bpy.types.Object, ...]) -> None:
+    """Translation of Blender's cube_project_exec function.
+
+    ./source/blender/editors/uvedit/uvedit_unwrap_ops.cc
+    Lines: ./4229-L4291"""
+    cube_size_init = cube_size
+    for idx, obj in enumerate(objects):
+        bm = bmesh.new()
+        bm.from_mesh(obj.data)
+
+        has_selection = any(f.select for f in bm.faces)
+
+        if not has_selection:
+            bm.free()
+            continue
+
+        # Declare bounds variable in advanced.
+        bounds: FloatBounds2x3 | None = None
+        center: FloatBounds3 = (0, 0, 0)
+
+        center, bounds = uv_map_transform_center(obj, bm, center)
+
+        if bounds:
+            dims = sub_v3_v3v3(bounds[1], bounds[0])
+            cube_size = max(dims)
+
+            if idx == 0:
+                # This doesn't fit well with multiple objects
+                cube_size_init = cube_size
+
+        uvedit_unwrap_cube_project(bm, cube_size_init, True, False, center)
+
+        per_face_aspect = True
+
+        uv_map_clip_correct(obj, bm, per_face_aspect)
+
+        bm.to_mesh(obj.data)
+        bm.free()
+        obj.data.update()
 
 
 def unwrap_instance_collection() -> None:
@@ -125,15 +416,7 @@ def unwrap_instance_collection() -> None:
         if obj.type != "MESH":
             continue
 
-        bm = bmesh.new()
-        bm.from_mesh(obj.data)
-        has_selection = any(f.select for f in bm.faces)
-
-        cube_project(bm, 1.0, has_selection, center=None)
-
-        bm.to_mesh(obj.data)
-        bm.free()
-        obj.data.update()
+        cube_project_exec(1.0, (obj,))
 
 
 def main() -> None:
@@ -145,15 +428,7 @@ def main() -> None:
             if obj.type != "MESH":
                 continue
 
-            bm = bmesh.new()
-            bm.from_mesh(obj.data)
-            has_selection = any(f.select for f in bm.faces)
-
-            cube_project(bm, 1.0, has_selection, center=None)
-
-            bm.to_mesh(obj.data)
-            bm.free()
-            obj.data.update()
+            cube_project_exec(1.0, (obj,))
 
             continue
 
